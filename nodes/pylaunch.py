0967b3020875ae0256319a94b91531dcd0edbd94
Shilpaj <shilpajbhalerao@gmail.com>
2020-09-24 01:57:20 +0530

    POC Dynamic ROS Nodes using Python API completed

--- nodes/pylaunch.py
+++ nodes/pylaunch.py
@@ -6,16 +6,22 @@ package = 'turtlesim-tf'
 executable = 'add_frame.py'
 # node = roslaunch.core.Node(package, executable, name="PyNode")
 
-node = roslaunch.core.Node(package, node_type=executable, name='Py', namespace='/',
-                           machine_name=None, args='',
-                           respawn=False, respawn_delay=0.0,
-                           remap_args=None, env_args=None, output=None, cwd=None,
-                           launch_prefix=None, required=False, filename=executable)
+nodes = []
+
+for i in range(5):
+    nodes.append(roslaunch.core.Node(package, node_type=executable, name='Py'+str(i), namespace='/',
+                                     machine_name=None, args='',
+                                     respawn=False, respawn_delay=0.0,
+                                     remap_args=None, env_args=None, output=None, cwd=None,
+                                     launch_prefix=None, required=False, filename=executable))
 
 launch = roslaunch.scriptapi.ROSLaunch()
 launch.start()
 
-process = launch.launch(node)
+for i in range(5):
+    process = launch.launch(nodes[i])
 time.sleep(15)
+print("Exit")
+exit()
 # print(process.is_alive())
-process.stop()
+# process.stop()
