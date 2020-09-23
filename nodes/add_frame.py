dcb653ede730e1aab4f4eb81b4afc8b006d770e0
Shilpaj <shilpajbhalerao@gmail.com>
2020-09-23 07:19:01 +0530

    Restructure the code

--- nodes/add_frame.py
+++ nodes/add_frame.py
@@ -10,15 +10,26 @@ from Turtle import *
 
 class Frame:
     def __init__(self):
-        rospy.init_node('Spawn')
-        reset_sim()
+        rospy.init_node('spawner', anonymous=False)
+
+        self.br = tf.TransformBroadcaster()
 
         self.x = 0.0
         self.y = 0.0
         self.theta = 0.0
 
+        self.random_spawn()
+
         self.br = tf.TransformBroadcaster()
 
+        print(rospy.get_param('turtle1/nth'))
+        self.rate = rospy.Rate(10.0)
+
+        while not rospy.is_shutdown():
+            self.dynamic_frame()
+            self.rate.sleep()
+
+    def random_spawn(self):
         collection = []
         for i in range(5):
             collection.append(Turtle(i))
@@ -27,8 +38,6 @@ class Frame:
 
             collection[i].spawn(self.x, self.y, self.theta)
             print(collection[i].get_name())
-
-        self.rate = rospy.Rate(10.0)
 
     def rand_pos(self):
         self.x = random.randint(0, 11)
@@ -46,23 +55,18 @@ class Frame:
     #                          "carrot1",
     #                          "turtle1")
     #         rate.sleep()
-    #
-    # def dynamic_frame(self):
-    #     rospy.init_node('dynamic_tf_broadcaster')
-    #     br = tf.TransformBroadcaster()
-    #     rate = rospy.Rate(10.0)
-    #     while not rospy.is_shutdown():
-    #         t = rospy.Time.now().to_sec() * math.pi
-    #         br.sendTransform((2.0 * math.sin(t), 2.0 * math.cos(t), 0.0),
-    #                          (0.0, 0.0, 0.0, 1.0),
-    #                          rospy.Time.now(),
-    #                          "carrot1",
-    #                          "turtle1")
-    #         rate.sleep()
+
+    def dynamic_frame(self):
+        t = rospy.Time.now().to_sec() * math.pi
+        self.br.sendTransform((2.0 * math.sin(t), 2.0 * math.cos(t), 0.0),
+                              (0.0, 0.0, 0.0, 1.0),
+                              rospy.Time.now(),
+                              "carrot1",
+                              "turtle1")
 
 
 if __name__ == '__main__':
     try:
-        Frame()
-    except Exception as e:
-        print(e)
+        turtle = Frame()
+    except KeyboardInterrupt:
+        exit()
