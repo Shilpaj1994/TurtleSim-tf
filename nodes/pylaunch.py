89e8c25554a8e819135ed7dd754f46efc72fd1aa
Shilpaj <shilpajbhalerao@gmail.com>
2020-09-24 19:51:57 +0530

    Created a Node class to control the node activities

--- nodes/pylaunch.py
+++ nodes/pylaunch.py
@@ -2,26 +2,56 @@
 import roslaunch
 import time
 
-package = 'turtlesim-tf'
-executable = 'add_frame.py'
-# node = roslaunch.core.Node(package, executable, name="PyNode")
 
-nodes = []
+class Node:
+    def __init__(self, name, package, executable, output):
+        self.name = name
+        self.package = package
+        self.executable = executable
+        self.output = output
+        self.process = None
 
-for i in range(5):
-    nodes.append(roslaunch.core.Node(package, node_type=executable, name='Py'+str(i), namespace='/',
-                                     machine_name=None, args='',
-                                     respawn=False, respawn_delay=0.0,
-                                     remap_args=None, env_args=None, output=None, cwd=None,
-                                     launch_prefix=None, required=False, filename=executable))
+        self._launch = roslaunch.scriptapi.ROSLaunch()
 
-launch = roslaunch.scriptapi.ROSLaunch()
-launch.start()
+        self._node = roslaunch.core.Node(package=self.package, node_type=self.executable, name=self.name, namespace='/',
+                                         machine_name=None, args='',
+                                         respawn=False, respawn_delay=0.0,
+                                         remap_args=None, env_args=None, output=None, cwd=None,
+                                         launch_prefix=None, required=False, filename=self.executable)
 
-for i in range(5):
-    process = launch.launch(nodes[i])
-time.sleep(15)
-print("Exit")
-exit()
-# print(process.is_alive())
-# process.stop()
+    def launch(self):
+        self._launch.start()
+        self.process = self._launch.launch(self._node)
+
+    def kill(self):
+        self.process.stop()
+
+    def status(self):
+        if self.process.is_alive():
+            return "Alive"
+        else:
+            return "Dead"
+    # def set_name(self, name):
+    #     self.name = name
+
+
+def main():
+    package = 'turtlesim-tf'
+    executable = 'add_frame.py'
+    output = 'screen'
+    name = 'Turtle'
+
+    node1 = Node(name, package, executable, output)
+
+    node1.launch()
+    print(node1.status())
+    time.sleep(10)
+    node1.kill()
+    print("END")
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
