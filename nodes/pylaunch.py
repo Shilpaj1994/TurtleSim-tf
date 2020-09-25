e713aa0c0d63d39911b1ea9ba26bfce0557efceb
Shilpaj <shilpajbhalerao@gmail.com>
2020-09-26 01:52:10 +0530

    Modified the pylauncher code

--- nodes/pylaunch.py
+++ nodes/pylaunch.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python
 import roslaunch
 import rospy
-import time
 import sys
 
 
 class Node:
-    def __init__(self, name_arg, package_arg, executable_arg, output_arg, args_arg=''):
-        self.name = name_arg
-        self.package = package_arg
-        self.executable = executable_arg
-        self.output = output_arg
-        self.args = args_arg
-        self.process = None
+    def __init__(self, name_arg, package_arg, executable_arg, instance_arg):
+        self._name = name_arg
+        self._package = package_arg
+        self._executable = executable_arg
+        self._instance_num = instance_arg
 
         self._launch = roslaunch.scriptapi.ROSLaunch()
+        self._process = None
+        self._node = None
 
-        self._node = roslaunch.core.Node(package=self.package, node_type=self.executable, name=self.name, namespace='/',
-                                         machine_name=None, args=self.args,
+    def define_node(self):
+        self._node = roslaunch.core.Node(package=self._package, node_type=self._executable, name=self._name, namespace='/',
+                                         machine_name=None, args=str(self._instance_num),
                                          respawn=False, respawn_delay=0.0,
-                                         remap_args=None, env_args=None, output=None, cwd=None,
-                                         launch_prefix=None, required=False, filename=self.executable)
+                                         remap_args=None, env_args=None, output='screen', cwd=None,
+                                         launch_prefix=None, required=False, filename='<unknown>')
 
     def launch(self):
         self._launch.start()
-        self.process = self._launch.launch(self._node)
+        self._process = self._launch.launch(self._node)
 
     def kill(self):
-        self.process.stop()
+        self._process.stop()
 
     def status(self):
-        if self.process.is_alive():
+        if self._process.is_alive():
             return "Alive"
         else:
             return "Dead"
@@ -49,37 +49,36 @@ def launch_sim():
     launch.launch(node)
 
 
-def main(number):
-    rospy.init_node("Starter")
+def main(number_of_nodes):
+    rospy.init_node("Launcher")
+    rate = rospy.Rate(15.0)
 
-    launch_sim()
+    node_collections = []
+    try:
+        launch_sim()
 
-    package = 'turtlesim-tf'
-    executable = 'add_frame.py'
-    output = "screen"
+        package = 'turtlesim-tf'
+        executable = 'add_frame.py'
 
-    nodes = []
+        for i in range(1, number_of_nodes+1):
+            name = 'Turtle' + str(i)
+            node_collections.append(Node(name_arg=name,
+                                         package_arg=package,
+                                         executable_arg=executable,
+                                         instance_arg=i))
+            node_collections[-1].define_node()
+            node_collections[-1].launch()
 
-    for i in range(number):
-        name = 'Turtle' + str(i)
-        nodes.append(Node(name_arg=name,
-                          package_arg=package,
-                          executable_arg=executable,
-                          output_arg=output))
-        nodes[-1].launch()
+        while not rospy.is_shutdown():
+            rate.sleep()
 
-    # for j in range(len(nodes)):
-    #     nodes[j].launch()
+        print("Exiting the process ....")
 
-    # node1.launch()
-    # print(node1.status())
-    time.sleep(10)
-    # node1.kill()
-    print("END")
+    except KeyboardInterrupt:
+        for j in range(len(node_collections)):
+            node_collections[-1].kill()
+        exit()
 
 
 if __name__ == '__main__':
-    try:
-        main(int(sys.argv[1]))
-    except KeyboardInterrupt:
-        exit()
+    main(int(sys.argv[1]))
